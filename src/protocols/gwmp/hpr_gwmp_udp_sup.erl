14ea513b3da713843e5fae0b905742eb00849639
Michael Jeffrey <michaeldjeffrey@gmail.com>
2022-08-16 18:20:43 -0700

    inline most of the udp sup

--- src/protocols/gwmp/hpr_gwmp_udp_sup.erl
+++ src/protocols/gwmp/hpr_gwmp_udp_sup.erl
@@ -8,14 +8,24 @@
 
 -behaviour(supervisor).
 
--include("gwmp_udp_worker_sup.hrl").
+-export([
+    start_link/0,
+    init/1,
+    maybe_start_worker/2,
+    lookup_worker/1
+]).
 
--export([start_link/0, init/1, maybe_start_worker/2, lookup_worker/1]).
-
--define(APP, helium_packet_router).
 -define(UDP_WORKER, hpr_gwmp_worker).
-
 -define(ETS, hpr_gwmp_udp_sup_ets).
+
+-define(WORKER(I), #{
+    id => I,
+    start => {I, start_link, []},
+    restart => temporary,
+    shutdown => 1000,
+    type => worker,
+    modules => [I]
+}).
 
 -define(FLAGS, #{
     strategy => simple_one_for_one,
@@ -23,72 +33,31 @@
     period => 60
 }).
 
+%%====================================================================
+%% API functions
+%%====================================================================
+
 start_link() ->
     supervisor:start_link({local, ?MODULE}, ?MODULE, []).
 
+-spec maybe_start_worker(WorkerKey :: binary(), Args :: map()) -> {ok, pid()} | {error, any()}.
 maybe_start_worker(WorkerKey, Args) ->
-    maybe_start_worker(WorkerKey, Args, ?APP, ?UDP_WORKER, ?ETS, ?MODULE).
-
-lookup_worker(WorkerKey) ->
-    lookup_worker(WorkerKey, ?ETS).
-
-init([]) ->
-    gwmp_udp_sup_init(?ETS, ?UDP_WORKER, ?FLAGS).
-
--spec maybe_start_worker(
-    PubKeyBin :: binary(),
-    Args :: map() | {error, any()},
-    AppName :: atom(),
-    UDPWorker :: atom(),
-    ETSTableName :: atom(),
-    SupModule :: atom()
-) -> {ok, pid()} | {error, any()} | {error, worker_not_started, any()}.
-maybe_start_worker(_WorkerKey, {error, _} = Err, _, _, _, _) ->
-    Err;
-maybe_start_worker(WorkerKey, Args, AppName, UDPWorker, ETSTableName, SupModule) ->
-    case ets:lookup(ETSTableName, WorkerKey) of
+    case ets:lookup(?ETS, WorkerKey) of
         [] ->
-            start_worker(WorkerKey, Args, AppName, UDPWorker, ETSTableName, SupModule);
+            start_worker(WorkerKey, Args);
         [{WorkerKey, Pid}] ->
             case erlang:is_process_alive(Pid) of
                 true ->
                     {ok, Pid};
                 false ->
-                    _ = ets:delete(ETSTableName, WorkerKey),
-                    start_worker(WorkerKey, Args, AppName, UDPWorker, ETSTableName, SupModule)
+                    _ = ets:delete(?ETS, WorkerKey),
+                    start_worker(WorkerKey, Args)
             end
     end.
 
--spec start_worker(
-    PubKeyBin :: binary(),
-    Args :: map(),
-    AppName :: atom(),
-    UDPWorker :: atom(),
-    ETSTableName :: atom(),
-    SupModule :: atom()
-) ->
-    {ok, pid()} | {error, worker_not_started, any()}.
-start_worker(PubKeyBin, Args, AppName, UDPWorker, ETSTableName, SupModule) ->
-    ChildArgs = maps:merge(#{pubkeybin => PubKeyBin}, Args),
-    case supervisor:start_child(SupModule, [ChildArgs]) of
-        {error, Err} ->
-            {error, worker_not_started, Err};
-        {ok, Pid} = OK ->
-            case ets:insert_new(ETSTableName, {PubKeyBin, Pid}) of
-                true ->
-                    OK;
-                false ->
-                    supervisor:terminate_child(UDPWorker, Pid),
-                    maybe_start_worker(PubKeyBin, Args, AppName, UDPWorker, ETSTableName, SupModule)
-            end
-    end.
-
--spec lookup_worker(
-    PubKeyBin :: binary(),
-    TabName :: atom()
-) -> {ok, pid()} | {error, not_found}.
-lookup_worker(WorkerKey, ETSTableName) ->
-    case ets:lookup(ETSTableName, WorkerKey) of
+-spec lookup_worker(PubKeyBin :: binary()) -> {ok, pid()} | {error, not_found}.
+lookup_worker(WorkerKey) ->
+    case ets:lookup(?ETS, WorkerKey) of
         [] ->
             {error, not_found};
         [{WorkerKey, Pid}] ->
@@ -98,6 +67,35 @@ lookup_worker(WorkerKey, ETSTableName) ->
             end
     end.
 
-gwmp_udp_sup_init(ETSTableName, UDPWorker, Flags) ->
-    ets:new(ETSTableName, [public, named_table, set]),
-    {ok, {Flags, [?WORKER(UDPWorker)]}}.
+%%====================================================================
+%% Supervisor callbacks
+%%====================================================================
+
+init([]) ->
+    ?ETS = ets:new(?ETS, [public, named_table, set]),
+    {ok, {?FLAGS, [?WORKER(?UDP_WORKER)]}}.
+
+%%====================================================================
+%% Internal Funtions
+%%====================================================================
+
+-spec start_worker(
+    PubKeyBin :: binary(),
+    Args :: map()
+) ->
+    {ok, pid()} | {error, worker_not_started, any()}.
+start_worker(PubKeyBin, Args) ->
+    ChildArgs = maps:merge(#{pubkeybin => PubKeyBin}, Args),
+    case supervisor:start_child(?MODULE, [ChildArgs]) of
+        {error, Err} ->
+            {error, worker_not_started, Err};
+        {ok, Pid} = OK ->
+            case ets:insert_new(?ETS, {PubKeyBin, Pid}) of
+                true ->
+                    OK;
+                false ->
+                    supervisor:terminate_child(?UDP_WORKER, Pid),
+                    maybe_start_worker(PubKeyBin, Args)
+            end
+    end.
+
