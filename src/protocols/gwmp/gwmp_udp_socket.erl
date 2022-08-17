af7c5a1482be073da3f5e01764351a12f433777e
Michael Jeffrey <michaeldjeffrey@gmail.com>
2022-08-16 17:10:47 -0700

    rename socket_info() -> socket_dest()
    implies more the data structures contents

--- src/protocols/gwmp/gwmp_udp_socket.erl
+++ src/protocols/gwmp/gwmp_udp_socket.erl
@@ -11,17 +11,17 @@
 
 -record(socket, {
     socket :: gen_udp:socket(),
-    primary :: socket_info() | undefined
+    primary :: socket_dest() | undefined
 }).
 
 -type socket() :: #socket{}.
 -type socket_address() :: inet:socket_address() | inet:hostname().
 -type socket_port() :: inet:port_number().
--type socket_info() :: {socket_address(), socket_port()}.
+-type socket_dest() :: {socket_address(), socket_port()}.
 
--export_type([socket/0, socket_address/0, socket_port/0]).
+-export_type([socket/0, socket_address/0, socket_port/0, socket_dest/0]).
 
--spec open(socket_info(), undefined) -> {ok, socket()}.
+-spec open(socket_dest(), undefined) -> {ok, socket()}.
 open(Primary, _Tee) ->
     {ok, Socket} = gen_udp:open(0, [binary, {active, true}]),
     {ok, #socket{socket = Socket, primary = Primary}}.
@@ -31,17 +31,17 @@ send(#socket{socket = Socket, primary = Primary}, Data) ->
     Reply = do_send(Socket, Primary, Data),
     Reply.
 
--spec do_send(gen_udp:socket(), undefined | socket_info(), binary()) -> ok | {error, any()}.
+-spec do_send(gen_udp:socket(), undefined | socket_dest(), binary()) -> ok | {error, any()}.
 do_send(_Socket, undefined, _Data) -> ok;
 do_send(Socket, {Address, Port}, Data) -> gen_udp:send(Socket, Address, Port, Data).
 
 close(#socket{socket = Socket}) ->
     gen_udp:close(Socket).
 
--spec update_address(socket(), socket_info()) -> {ok, socket()}.
+-spec update_address(socket(), socket_dest()) -> {ok, socket()}.
 update_address(#socket{} = Socket, SocketInfo) ->
     {ok, Socket#socket{primary = SocketInfo}}.
 
--spec get_address(socket()) -> socket_info().
+-spec get_address(socket()) -> socket_dest().
 get_address(#socket{primary = Primary}) ->
     Primary.
