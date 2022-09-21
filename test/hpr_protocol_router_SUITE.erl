4dd22a11cf010262b79f1c13a83341d2f538c5b6
Michael Jeffrey <michaeldjeffrey@gmail.com>
2022-09-20 17:15:56 -0700

    more ct tests for grpc protocol

--- test/hpr_protocol_router_SUITE.erl
+++ test/hpr_protocol_router_SUITE.erl
@@ -7,7 +7,10 @@
 ]).
 
 -export([
-    grpc_test/1
+    grpc_test/1,
+    grpc_connection_refused_test/1,
+    grpc_full_flow_send_test/1,
+    grpc_full_flow_connection_refused_test/1
 ]).
 
 -include_lib("eunit/include/eunit.hrl").
@@ -24,7 +27,10 @@
 %%--------------------------------------------------------------------
 all() ->
     [
-        grpc_test
+        grpc_test,
+        grpc_connection_refused_test,
+        grpc_full_flow_send_test,
+        grpc_full_flow_connection_refused_test
     ].
 
 %%--------------------------------------------------------------------
@@ -44,38 +50,19 @@ end_per_testcase(TestCase, Config) ->
 %%--------------------------------------------------------------------
 
 grpc_test(_Config) ->
-    _PacketUp =
-        {packet_router_packet_up_v1_pb,
-            <<0, 55, 148, 187, 74, 220, 108, 33, 11, 133, 65, 148, 104, 147, 177, 26, 124, 43, 169,
-                155, 182, 228, 95>>,
-            552140, 0, 904.7000122070312, 'SF10BW125', 5.5, 'US915', 0,
-            <<1, 131, 45, 83, 233, 204, 91, 71, 221, 213, 157, 129, 213, 219, 31, 62, 233, 186, 49,
-                28, 183, 141, 0, 52, 52, 175, 168, 41, 37, 8, 230, 89, 83>>,
-            <<192, 249, 98, 190, 86, 74, 255, 124, 33, 190, 95, 141, 9, 173, 111, 180, 97, 42, 203,
-                136, 11, 36, 144, 4, 128, 6, 190, 253, 153, 99, 3, 180, 161, 76, 88, 90, 106, 200,
-                160, 115, 22, 81, 211, 37, 134, 14, 8, 99, 226, 1, 172, 157, 67, 170, 203, 246, 21,
-                250, 191, 236, 93, 230, 221, 9>>},
+    PacketUp = test_packet(),
+    Route = test_route(),
 
-    _Route =
-        {packet_router_route_v1_pb, 12582995,
-            [{packet_router_route_devaddr_range_v1_pb, 0, 4294967295}],
-            [{packet_router_route_eui_v1_pb, 802041902051071031, 8942655256770396549}],
-            <<"127.0.0.1:8082">>, router, 4020},
+    %% Startup a test server
+    {ok, ServerPid} = grpcbox:start_server(#{
+        grpc_opts => #{
+            service_protos => [packet_router_pb],
+            services => #{'helium.packet_router.gateway' => test_hpr_gateway_service}
+        },
+        listen_opts => #{port => 8082, ip => {0, 0, 0, 0}}
+    }),
 
-    ?assertMatch(
-        {ok, _ServerPid},
-        grpcbox:start_server(#{
-            grpc_opts => #{
-                service_protos => [packet_router_pb],
-                services => #{'helium.packet_router.gateway' => test_hpr_gateway_service}
-            },
-            listen_opts => #{
-                port => 8082,
-                ip => {0, 0, 0, 0}
-            }
-        })
-    ),
-
+    %% Interceptor
     Self = self(),
     application:set_env(
         hpr,
@@ -86,12 +73,160 @@ grpc_test(_Config) ->
         end
     ),
 
-    ok = hpr_protocol_router:send(_PacketUp, self(), _Route),
+    %% Send packet and route directly through interface
+    _ = hpr_protocol_router:send(PacketUp, self(), Route),
 
     ok =
         receive
-            {test_send_packet, _Packet} -> ?assertEqual(_Packet, _PacketUp)
+            {test_send_packet, _Packet} -> ?assertEqual(_Packet, PacketUp)
         after timer:seconds(2) -> ct:fail(no_msg_rcvd)
         end,
 
+    ok = gen_server:stop(ServerPid),
+
     ok.
+
+grpc_connection_refused_test(_Config) ->
+    PacketUp = test_packet(),
+    Route = test_route(),
+
+    ?assertEqual(
+        {error, econnrefused},
+        hpr_protocol_router:send(PacketUp, self(), Route)
+    ),
+
+    ok.
+
+grpc_full_flow_send_test(_Congig) ->
+    Packet = test_packet(),
+    PacketMap = hpr_packet_up:to_map(Packet),
+    %% Startup our server
+    {ok, ServerPid} = grpcbox:start_server(#{
+        grpc_opts => #{
+            service_protos => [packet_router_pb],
+            services => #{'helium.packet_router.gateway' => hpr_gateway_service}
+        },
+        listen_opts => #{port => 8080, ip => {0, 0, 0, 0}}
+    }),
+
+    %% Startup test server for receiving
+    {ok, TestServerPid} = grpcbox:start_server(#{
+        grpc_opts => #{
+            service_protos => [packet_router_pb],
+            services => #{'helium.packet_router.gateway' => test_hpr_gateway_service}
+        },
+        listen_opts => #{port => 8082, ip => {0, 0, 0, 0}}
+    }),
+
+    %% Forward messages to ourselves
+    Self = self(),
+    application:set_env(
+        hpr,
+        gateway_service_send_packet_fun,
+        fun(PacketUp, Socket) ->
+            Self ! {test_send_packet, PacketUp},
+            Socket
+        end
+    ),
+
+    %% Insert the matching route for the test packet
+    ok = hpr_routing_config_worker:insert(test_route()),
+    meck:new(hpr_gateway_service, [passthrough]),
+
+    {ok, Stream} = helium_packet_router_gateway_client:send_packet(ctx:new()),
+    ok = grpcbox_client:send(Stream, PacketMap),
+
+    ok =
+        receive
+            {test_send_packet, P} -> ?assertEqual(P, Packet)
+        after 250 -> ct:fail(no_packet_delivered)
+        end,
+
+    ?assertEqual(
+        1,
+        meck:num_calls(hpr_gateway_service, send_packet, '_'),
+        "we should only attempt to send a packet 1 time"
+    ),
+
+    %% ===================================================================
+    %% We're stopping the test server to make sure we don't try to deliver
+    %% multiple times for a connection we cannot make or has gone down.
+    %% Also, resetting the mock to make sure send_packet is called once.
+    ok = gen_server:stop(TestServerPid),
+    ok = meck:reset(hpr_gateway_service),
+
+    ok = grpcbox_client:send(Stream, PacketMap),
+    ok =
+        receive
+            {test_send_packet, _} -> ct:fail(expected_no_packet)
+        after 250 -> ok
+        end,
+
+    ?assertEqual(
+        1,
+        meck:num_calls(hpr_gateway_service, send_packet, '_'),
+        "we should only attempt to senda packet 1 time, even if it failed"
+    ),
+
+    ok = gen_server:stop(ServerPid),
+    ok = meck:unload(hpr_gateway_service),
+
+    ok.
+
+grpc_full_flow_connection_refused_test(_Config) ->
+    Packet = test_packet(),
+    PacketMap = hpr_packet_up:to_map(Packet),
+    %% Startup our server
+    {ok, ServerPid} = grpcbox:start_server(#{
+        grpc_opts => #{
+            service_protos => [packet_router_pb],
+            services => #{'helium.packet_router.gateway' => hpr_gateway_service}
+        },
+        listen_opts => #{port => 8080, ip => {0, 0, 0, 0}}
+    }),
+
+    %% Insert the matching route for the test packet
+    ok = hpr_routing_config_worker:insert(test_route()),
+    {ok, Stream} = helium_packet_router_gateway_client:send_packet(ctx:new()),
+
+    %% ===================================================================
+    %% The test server is not started in this test. I'm not sure this test is
+    %% accurate yet. But it is here for when we understand more about why
+    %% grpcbox recalls its service handler when something fails.
+
+    meck:new(hpr_gateway_service, [passthrough]),
+    ok = grpcbox_client:send(Stream, PacketMap),
+    ok =
+        receive
+            {test_send_packet, _} -> ct:fail(expected_no_packet)
+        after 250 -> ok
+        end,
+
+    ?assertEqual(1, meck:num_calls(hpr_gateway_service, send_packet, '_')),
+
+    ok = gen_server:stop(ServerPid),
+    ok = meck:unload(hpr_gateway_service),
+
+    ok.
+
+%% ===================================================================
+%% Helpers
+%% ===================================================================
+
+test_packet() ->
+    {packet_router_packet_up_v1_pb,
+        <<0, 55, 148, 187, 74, 220, 108, 33, 11, 133, 65, 148, 104, 147, 177, 26, 124, 43, 169, 155,
+            182, 228, 95>>,
+        552140, 0, 904.7000122070312, 'SF10BW125', 5.5, 'US915', 0,
+        <<1, 131, 45, 83, 233, 204, 91, 71, 221, 213, 157, 129, 213, 219, 31, 62, 233, 186, 49, 28,
+            183, 141, 0, 52, 52, 175, 168, 41, 37, 8, 230, 89, 83>>,
+        <<192, 249, 98, 190, 86, 74, 255, 124, 33, 190, 95, 141, 9, 173, 111, 180, 97, 42, 203, 136,
+            11, 36, 144, 4, 128, 6, 190, 253, 153, 99, 3, 180, 161, 76, 88, 90, 106, 200, 160, 115,
+            22, 81, 211, 37, 134, 14, 8, 99, 226, 1, 172, 157, 67, 170, 203, 246, 21, 250, 191, 236,
+            93, 230, 221, 9>>}.
+
+test_route() ->
+    {packet_router_route_v1_pb, 12582995,
+        [{packet_router_route_devaddr_range_v1_pb, 0, 4294967295}],
+        [{packet_router_route_eui_v1_pb, 802041902051071031, 8942655256770396549}],
+        <<"127.0.0.1:8082">>, router, 4020}.
