ec93989086e731565ca28f045e42eb362b940984
Michael Jeffrey <michaeldjeffrey@gmail.com>
2023-02-23 16:51:02 -0700

    update roaming register to remove signer

--- src/grpc/downlink/hpr_http_roaming_register.erl
+++ src/grpc/downlink/hpr_http_roaming_register.erl
@@ -3,10 +3,9 @@
 -include("../autogen/downlink_pb.hrl").
 
 -export([
-    new/2,
+    new/1,
     region/1,
     timestamp/1,
-    signer/1,
     signature/1,
     sign/2,
     verify/1
@@ -16,12 +15,11 @@
 
 -export_type([http_roaming_register/0]).
 
--spec new(Region :: atom(), Signer :: libp2p_crypto:pubkey_bin()) -> http_roaming_register().
-new(Region, Signer) ->
+-spec new(Region :: atom()) -> http_roaming_register().
+new(Region) ->
     #http_roaming_register_v1_pb{
         region = Region,
-        timestamp = erlang:system_time(millisecond),
-        signer = Signer
+        timestamp = erlang:system_time(millisecond)
     }.
 
 -spec region(HttpRoamingReg :: http_roaming_register()) -> atom().
@@ -31,10 +29,6 @@ region(HttpRoamingReg) ->
 -spec timestamp(HttpRoamingReg :: http_roaming_register()) -> non_neg_integer().
 timestamp(HttpRoamingReg) ->
     HttpRoamingReg#http_roaming_register_v1_pb.timestamp.
-
--spec signer(HttpRoamingReg :: http_roaming_register()) -> libp2p_crypto:pubkey_bin().
-signer(HttpRoamingReg) ->
-    HttpRoamingReg#http_roaming_register_v1_pb.signer.
 
 -spec signature(HttpRoamingReg :: http_roaming_register()) -> binary().
 signature(HttpRoamingReg) ->
@@ -75,37 +69,27 @@ region_test() ->
     Region = 'EU868',
     ?assertEqual(
         Region,
-        ?MODULE:region(?MODULE:new(Region, <<"Signer">>))
+        ?MODULE:region(?MODULE:new(Region))
     ),
     ok.
 
 timestamp_test() ->
     Signer = <<"Signer">>,
     Timestamp = erlang:system_time(millisecond),
-    ?assert(Timestamp =< ?MODULE:timestamp(?MODULE:new('EU868', Signer))),
-    ok.
-
-signer_test() ->
-    Signer = <<"Signer">>,
-    ?assertEqual(
-        Signer,
-        ?MODULE:signer(?MODULE:new('EU868', Signer))
-    ),
+    ?assert(Timestamp =< ?MODULE:timestamp(?MODULE:new('EU868')),
     ok.
 
 signature_test() ->
-    Signer = <<"Signer">>,
     ?assertEqual(
         <<>>,
-        ?MODULE:signature(?MODULE:new('EU868', Signer))
+        ?MODULE:signature(?MODULE:new('EU868'))
     ),
     ok.
 
 sign_verify_test() ->
     #{public := PubKey, secret := PrivKey} = libp2p_crypto:generate_keys(ecc_compact),
     SigFun = libp2p_crypto:mk_sig_fun(PrivKey),
-    Signer = libp2p_crypto:pubkey_to_bin(PubKey),
-    HttpRoamingReg = ?MODULE:new('EU868', Signer),
+    HttpRoamingReg = ?MODULE:new('EU868'),
 
     SignedHttpRoamingReg = ?MODULE:sign(HttpRoamingReg, SigFun),
 
