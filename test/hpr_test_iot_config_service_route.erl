45db0002c229c9a3aca7129fe6b052662ac44016
Jeff Grunewald <jeff.grunewald@gmail.com>
2023-04-18 17:48:58 -0400

    reintroduce signer pubkeybin proto field (#174)
    * reintroduce signer pubkeybin proto field
    
    * reset proto dep to master

--- test/hpr_test_iot_config_service_route.erl
+++ test/hpr_test_iot_config_service_route.erl
@@ -2,8 +2,6 @@
 
 -behaviour(helium_iot_config_route_bhvr).
 
--include("hpr.hrl").
-
 -export([
     init/2,
     handle_info/2
@@ -25,31 +23,20 @@
 ]).
 
 -export([
-    stream_resp/1,
-    set_signer/1
+    stream_resp/1
 ]).
-
--record(state, {
-    signer :: undefined | libp2p_crypto:pubkey_bin()
-}).
 
 -spec init(atom(), StreamState :: grpcbox_stream:t()) -> grpcbox_stream:t().
 init(_RPC, StreamState) ->
     Self = self(),
     true = erlang:register(?MODULE, self()),
-    {PubKey, _SigFun} = persistent_term:get(?HPR_KEY, {undefined, undefined}),
-    PubKeyBin = libp2p_crypto:pubkey_to_bin(PubKey),
-    lager:notice("init ~p @ ~p with signer ~p", [?MODULE, Self, PubKeyBin]),
-    grpcbox_stream:stream_handler_state(StreamState, #state{signer = PubKeyBin}).
+    lager:notice("init ~p @ ~p", [?MODULE, Self]),
+    StreamState.
 
 -spec handle_info(Msg :: any(), StreamState :: grpcbox_stream:t()) -> grpcbox_stream:t().
 handle_info({stream_resp, RouteStreamResp}, StreamState) ->
     lager:notice("got RouteStreamResp ~p", [RouteStreamResp]),
     grpcbox_stream:send(false, RouteStreamResp, StreamState);
-handle_info({set_signer, Signer}, StreamState) ->
-    lager:notice("got new Signer ~p @ ~p", [Signer, ?MODULE]),
-    HandlerState = grpcbox_stream:stream_handler_state(StreamState),
-    grpcbox_stream:stream_handler_state(StreamState, HandlerState#state{signer = Signer});
 handle_info(_Msg, StreamState) ->
     StreamState.
 
@@ -87,9 +74,7 @@ delete_devaddr_ranges(_Ctx, _Msg) ->
     {grpc_error, {12, <<"UNIMPLEMENTED">>}}.
 
 stream(RouteStreamReq, StreamState) ->
-    HandlerState = grpcbox_stream:stream_handler_state(StreamState),
-    Signer = HandlerState#state.signer,
-    case hpr_route_stream_req:verify(RouteStreamReq, Signer) of
+    case hpr_route_stream_req:verify(RouteStreamReq) of
         false ->
             {grpc_error, {7, <<"PERMISSION_DENIED">>}};
         true ->
@@ -100,10 +85,4 @@ stream(RouteStreamReq, StreamState) ->
 stream_resp(RouteStreamResp) ->
     lager:notice("stream_resp ~p  @ ~p", [RouteStreamResp, erlang:whereis(?MODULE)]),
     ?MODULE ! {stream_resp, RouteStreamResp},
-    ok.
-
--spec set_signer(Signer :: libp2p_crypto:pubkey_bin()) -> ok.
-set_signer(Signer) ->
-    lager:notice("set_signer ~p @ ~p", [Signer, erlang:whereis(?MODULE)]),
-    ?MODULE ! {set_signer, Signer},
     ok.
--- test/hpr_test_iot_config_service_skf.erl
+++ test/hpr_test_iot_config_service_skf.erl
@@ -2,8 +2,6 @@
 
 -behaviour(helium_iot_config_session_key_filter_bhvr).
 
--include("hpr.hrl").
-
 -export([
     init/2,
     handle_info/2
@@ -19,31 +17,20 @@
 ]).
 
 -export([
-    stream_resp/1,
-    set_signer/1
+    stream_resp/1
 ]).
-
--record(state, {
-    signer :: undefined | libp2p_crypto:pubkey_bin()
-}).
 
 -spec init(atom(), StreamState :: grpcbox_stream:t()) -> grpcbox_stream:t().
 init(_RPC, StreamState) ->
     Self = self(),
     true = erlang:register(?MODULE, self()),
-    {PubKey, _SigFun} = persistent_term:get(?HPR_KEY, {undefined, undefined}),
-    PubKeyBin = libp2p_crypto:pubkey_to_bin(PubKey),
-    lager:notice("init ~p @ ~p with signer ~p", [?MODULE, Self, PubKeyBin]),
-    grpcbox_stream:stream_handler_state(StreamState, #state{signer = PubKeyBin}).
+    lager:notice("init ~p @ ~p", [?MODULE, Self]),
+    StreamState.
 
 -spec handle_info(Msg :: any(), StreamState :: grpcbox_stream:t()) -> grpcbox_stream:t().
 handle_info({stream_resp, SKFStreamResp}, StreamState) ->
     lager:notice("got SKFStreamResp ~p", [SKFStreamResp]),
     grpcbox_stream:send(false, SKFStreamResp, StreamState);
-handle_info({set_signer, Signer}, StreamState) ->
-    lager:notice("got new Signer ~p @ ~p", [Signer, ?MODULE]),
-    HandlerState = grpcbox_stream:stream_handler_state(StreamState),
-    grpcbox_stream:stream_handler_state(StreamState, HandlerState#state{signer = Signer});
 handle_info(_Msg, StreamState) ->
     StreamState.
 
@@ -64,9 +51,7 @@ delete(_Ctx, _SKFListReq) ->
 
 stream(SKFStreamReq, StreamState) ->
     lager:notice("stream_req ~p", [SKFStreamReq]),
-    HandlerState = grpcbox_stream:stream_handler_state(StreamState),
-    Signer = HandlerState#state.signer,
-    case hpr_skf_stream_req:verify(SKFStreamReq, Signer) of
+    case hpr_skf_stream_req:verify(SKFStreamReq) of
         false ->
             lager:notice("PERMISSION_DENIED"),
             {grpc_error, {7, <<"PERMISSION_DENIED">>}};
@@ -81,10 +66,4 @@ stream(SKFStreamReq, StreamState) ->
 stream_resp(SKFStreamResp) ->
     lager:notice("stream_resp ~p  @ ~p", [SKFStreamResp, erlang:whereis(?MODULE)]),
     ?MODULE ! {stream_resp, SKFStreamResp},
-    ok.
-
--spec set_signer(Signer :: libp2p_crypto:pubkey_bin()) -> ok.
-set_signer(Signer) ->
-    lager:notice("set_signer ~p @ ~p", [Signer, erlang:whereis(?MODULE)]),
-    ?MODULE ! {set_signer, Signer},
     ok.
