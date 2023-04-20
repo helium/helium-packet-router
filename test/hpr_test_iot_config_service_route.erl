6a6ecc38fb37d7732e44f144425d666ed35b88fd
Macpie <pierre@nova-labs.com>
2023-04-20 15:12:43 -0700

    Fixup error return in grpc handlers

--- test/hpr_test_iot_config_service_route.erl
+++ test/hpr_test_iot_config_service_route.erl
@@ -46,54 +46,54 @@ handle_info(_Msg, StreamState) ->
     StreamState.
 
 list(_Ctx, _Msg) ->
-    {grpc_error, {12, <<"UNIMPLEMENTED">>}}.
+    {grpc_error, {grpcbox_stream:code_to_status(12), <<"UNIMPLEMENTED">>}}.
 
 get(_Ctx, _Msg) ->
-    {grpc_error, {12, <<"UNIMPLEMENTED">>}}.
+    {grpc_error, {grpcbox_stream:code_to_status(12), <<"UNIMPLEMENTED">>}}.
 
 create(_Ctx, _Msg) ->
-    {grpc_error, {12, <<"UNIMPLEMENTED">>}}.
+    {grpc_error, {grpcbox_stream:code_to_status(12), <<"UNIMPLEMENTED">>}}.
 
 update(_Ctx, _Msg) ->
-    {grpc_error, {12, <<"UNIMPLEMENTED">>}}.
+    {grpc_error, {grpcbox_stream:code_to_status(12), <<"UNIMPLEMENTED">>}}.
 
 delete(_Ctx, _Msg) ->
-    {grpc_error, {12, <<"UNIMPLEMENTED">>}}.
+    {grpc_error, {grpcbox_stream:code_to_status(12), <<"UNIMPLEMENTED">>}}.
 
 stream(RouteStreamReq, StreamState) ->
     case hpr_route_stream_req:verify(RouteStreamReq) of
         false ->
-            {grpc_error, {7, <<"PERMISSION_DENIED">>}};
+            {grpc_error, {grpcbox_stream:code_to_status(7), <<"PERMISSION_DENIED">>}};
         true ->
             {ok, StreamState}
     end.
 
 get_euis(_Msg, _Stream) ->
-    {grpc_error, {12, <<"UNIMPLEMENTED">>}}.
+    {grpc_error, {grpcbox_stream:code_to_status(12), <<"UNIMPLEMENTED">>}}.
 
 update_euis(_Msg, _Stream) ->
-    {grpc_error, {12, <<"UNIMPLEMENTED">>}}.
+    {grpc_error, {grpcbox_stream:code_to_status(12), <<"UNIMPLEMENTED">>}}.
 
 delete_euis(_Ctx, _Msg) ->
-    {grpc_error, {12, <<"UNIMPLEMENTED">>}}.
+    {grpc_error, {grpcbox_stream:code_to_status(12), <<"UNIMPLEMENTED">>}}.
 
 get_devaddr_ranges(_Msg, _Stream) ->
-    {grpc_error, {12, <<"UNIMPLEMENTED">>}}.
+    {grpc_error, {grpcbox_stream:code_to_status(12), <<"UNIMPLEMENTED">>}}.
 
 update_devaddr_ranges(_Msg, _Stream) ->
-    {grpc_error, {12, <<"UNIMPLEMENTED">>}}.
+    {grpc_error, {grpcbox_stream:code_to_status(12), <<"UNIMPLEMENTED">>}}.
 
 delete_devaddr_ranges(_Ctx, _Msg) ->
-    {grpc_error, {12, <<"UNIMPLEMENTED">>}}.
+    {grpc_error, {grpcbox_stream:code_to_status(12), <<"UNIMPLEMENTED">>}}.
 
 get_skfs(_Ctx, _Msg) ->
-    {grpc_error, {12, <<"UNIMPLEMENTED">>}}.
+    {grpc_error, {grpcbox_stream:code_to_status(12), <<"UNIMPLEMENTED">>}}.
 
 list_skfs(_Ctx, _Msg) ->
-    {grpc_error, {12, <<"UNIMPLEMENTED">>}}.
+    {grpc_error, {grpcbox_stream:code_to_status(12), <<"UNIMPLEMENTED">>}}.
 
 update_skfs(_Ctx, _Msg) ->
-    {grpc_error, {12, <<"UNIMPLEMENTED">>}}.
+    {grpc_error, {grpcbox_stream:code_to_status(12), <<"UNIMPLEMENTED">>}}.
 
 -spec stream_resp(RouteStreamResp :: hpr_route_stream_res:res()) -> ok.
 stream_resp(RouteStreamResp) ->
