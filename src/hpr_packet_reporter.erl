258dc1663d709520f0f114a3402662e9f4ceda98
Macpie <pierre@nova-labs.com>
2024-02-26 15:18:18 -0800

    Implement HIP 102

--- src/hpr_packet_reporter.erl
+++ src/hpr_packet_reporter.erl
@@ -2,6 +2,8 @@
 
 -behaviour(gen_server).
 
+-include("hpr.hrl").
+
 %% ------------------------------------------------------------------
 %% API Function Exports
 %% ------------------------------------------------------------------
@@ -164,13 +166,17 @@ terminate(_Reason, #state{current_packets = Packets}) ->
 
 -spec encode_packet(
     Packet :: hpr_packet_up:packet(),
-    PacketRoute :: hpr_route:route(),
-    IsFree :: boolean(),
+    Route :: hpr_route:route(),
+    IsFree0 :: boolean(),
     ReceivedTime :: non_neg_integer()
 ) -> binary().
-encode_packet(Packet, PacketRoute, IsFree, ReceivedTime) ->
+encode_packet(Packet, Route, IsFree0, ReceivedTime) ->
+    NetID = hpr_route:net_id(Route),
+    NetIDStr = hpr_utils:int_to_hex_string(NetID),
+    FreeNetIDs = application:get_env(?APP, ?HPR_FREE_NET_IDS, []),
+    IsFree1 = IsFree0 orelse lists:member(NetIDStr, FreeNetIDs),
     EncodedPacket = hpr_packet_report:encode(
-        hpr_packet_report:new(Packet, PacketRoute, IsFree, ReceivedTime)
+        hpr_packet_report:new(Packet, Route, IsFree1, ReceivedTime)
     ),
     PacketSize = erlang:size(EncodedPacket),
     <<PacketSize:32/big-integer-unsigned, EncodedPacket/binary>>.
